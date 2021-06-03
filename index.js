037c198f6540745cf93563369562b9ff1a27b5d2
Gordon Hodanich <ghodanich@gmail.com>
2021-06-03 15:50:09 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+function submitData(userName, userEmail){
+    let url = 'http://localhost:3000/users'
+    let users = {
+        name: userName,
+        email: userEmail
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(users)
+    }
+    return fetch(url, configObj)
+    .then(function(response){
+        return response.json()
+    })
+    .then (function(object){
+        console.log(object)
+        document.body.innerHTML = object.id
+    })
+    .catch(function(error){
+        alert("Uh oh, stay calm")
+        document.body.innerHTML = error.message
+    })
+}
